2753dbd625df97a695dd7531f84ab07e329c8358
Joel Blaser <joel.blaser.3@gmail.com>
2021-10-11 22:36:30 +0200

    feat(login): added login form

--- src/components/page-login/PageLogin.tsx
+++ src/components/page-login/PageLogin.tsx
@@ -1,3 +1,51 @@
+import './PageLogin.scss';
+import Card from '@mui/material/Card';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { useForm } from 'src/hooks/useForm';
+import { Auth } from 'src/models/auth.model';
+
 export function PageLogin() {
-  return <div></div>;
+  const auth = getAuth();
+
+  const onLogin = () => {
+    signInWithEmailAndPassword(auth, user.email, user.password)
+      .then((value) => {
+        console.log(value.user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    clearForm();
+  };
+
+  const [user, handleChange, handleSubmit, clearForm] = useForm<Auth>(onLogin);
+
+  return (
+    <div className="page-login">
+      <Card className="login-card">
+        <form onSubmit={handleSubmit}>
+          <TextField
+            label="Email"
+            variant="outlined"
+            name="email"
+            value={user.email || ''}
+            onChange={handleChange}
+          />
+          <TextField
+            label="Password"
+            type="password"
+            variant="outlined"
+            name="password"
+            value={user.password || ''}
+            onChange={handleChange}
+          />
+          <Button variant="contained" type="submit">
+            Login
+          </Button>
+        </form>
+      </Card>
+    </div>
+  );
 }
