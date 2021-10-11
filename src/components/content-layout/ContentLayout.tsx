072f18cd4b8d34a3851b302ed4d93a2ba465110b
Joel Blaser <joel.blaser.3@gmail.com>
2021-10-11 21:36:39 +0200

    feat(header): updated header

--- src/components/content-layout/ContentLayout.tsx
+++ src/components/content-layout/ContentLayout.tsx
@@ -2,6 +2,7 @@ import './ContentLayout.scss';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Header } from '../header/Header';
 import { SignUp } from '../sign-up/SignUp';
+import { PageLogin } from '../page-login/PageLogin';
 
 export function ContentLayout() {
   return (
@@ -9,6 +10,9 @@ export function ContentLayout() {
       <Header />
       <div className="content">
         <BrowserRouter>
+          <Route path="/login">
+            <PageLogin />
+          </Route>
           <Route path="/register">
             <SignUp />
           </Route>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,15 @@
-import AppBar from '@mui/material/AppBar';
+import './Header.scss';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 export function Header() {
   return (
-    <AppBar>
-      <Toolbar>
-        <Typography variant="h6" component="div">
-          Quiz App
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <Toolbar className="header">
+      <Typography variant="h6" component="div">
+        Quiz App
+      </Typography>
+      <Button variant="contained" className="login-button">Login</Button>
+    </Toolbar>
   );
 }
