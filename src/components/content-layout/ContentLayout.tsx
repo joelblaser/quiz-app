798b1d9e8ea3a49071fb5e1fe09f689a788094df
Joel Blaser <joel.blaser.3@gmail.com>
2021-10-13 12:01:37 +0200

    feat(header): added navigation to login page

--- src/components/content-layout/ContentLayout.tsx
+++ src/components/content-layout/ContentLayout.tsx
@@ -6,18 +6,18 @@ import { PageLogin } from '../page-login/PageLogin';
 
 export function ContentLayout() {
   return (
-    <div>
-      <Header />
-      <div className="content">
-        <BrowserRouter>
-          <Route path="/login">
-            <PageLogin />
-          </Route>
-          <Route path="/register">
-            <SignUp />
-          </Route>
-        </BrowserRouter>
-      </div>
+    <div className="content">
+      <BrowserRouter>
+        <Route path="/">
+          <Header />
+        </Route>
+        <Route path="/login">
+          <PageLogin />
+        </Route>
+        <Route path="/register">
+          <SignUp />
+        </Route>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,14 +2,25 @@ import './Header.scss';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { useHistory } from 'react-router-dom';
 
 export function Header() {
+  const history = useHistory();
+
+  const goToLogin = () => {
+    if (history.location.pathname !== '/login') {
+      history.push('/login');
+    }
+  };
+
   return (
     <Toolbar className="header">
       <Typography variant="h6" component="div">
         Quiz App
       </Typography>
-      <Button variant="contained" className="login-button">Login</Button>
+      <Button variant="contained" className="login-button" onClick={goToLogin}>
+        Login
+      </Button>
     </Toolbar>
   );
 }
