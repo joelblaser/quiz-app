e46395946f539b224fc19a05b2a3d056bcc2eeb5
Joel Blaser <joel.blaser.3@gmail.com>
2021-10-16 15:31:55 +0200

    feat(header): adjusted header buttons to auth state

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,24 +1,58 @@
 import './Header.scss';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { useRouter } from 'src/hooks/useRouter';
+import { getAuth, onAuthStateChanged, signOut } from '@firebase/auth';
+import { useState } from 'react';
 
 export function Header() {
+  const auth = getAuth();
   const router = useRouter();
+
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
+  onAuthStateChanged(auth, (user) => {
+    setIsUserLoggedIn(user ? true : false);
+  });
 
   const goToLogin = () => {
     router.navigate('/login');
   };
 
+  const goToRegister = () => {
+    router.navigate('/register');
+  };
+
+  const logout = () => {
+    signOut(auth).then(() => {
+      router.navigate('/');
+    });
+  };
+
   return (
-    <Toolbar className="header">
-      <Typography variant="h6" component="div">
+    <div className="header">
+      <Typography variant="h5" component="div" className="title">
         Quiz App
       </Typography>
-      <Button variant="contained" className="login-button" onClick={goToLogin}>
-        Login
-      </Button>
-    </Toolbar>
+      {!isUserLoggedIn && (
+        <Button variant="outlined" className="right-button" onClick={goToLogin}>
+          Login
+        </Button>
+      )}
+      {!isUserLoggedIn && (
+        <Button
+          variant="contained"
+          className="right-button"
+          onClick={goToRegister}
+        >
+          Register
+        </Button>
+      )}
+      {isUserLoggedIn && (
+        <Button variant="contained" className="right-button" onClick={logout}>
+          Logout
+        </Button>
+      )}
+    </div>
   );
 }
