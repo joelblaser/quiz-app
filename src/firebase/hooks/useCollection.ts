e6bd2bf0caefbdc7145231b3efd1297e2b34267c
Joel Blaser <joel.blaser.3@gmail.com>
2021-10-16 11:53:28 +0200

    refactor(firebase): refactored hooks

--- src/firebase/hooks/useCollection.ts
+++ src/firebase/hooks/useCollection.ts
@@ -3,14 +3,14 @@ import { collection, query } from 'firebase/firestore';
 import { firestore } from '../app.firebase';
 import { useQuery } from './useQuery';
 
-export function useCollection<T>(name: string) {
+export function useCollection<T>(name: string): T[] {
   const [q, setQuery] = useState(query(collection(firestore, name)));
 
-  const [data] = useQuery<T>(q);
+  const data = useQuery<T>(q);
 
   useEffect(() => {
     setQuery(query(collection(firestore, name)));
   }, [name]);
 
-  return [data] as [T];
+  return data;
 }
--- src/firebase/hooks/useQuery.ts
+++ src/firebase/hooks/useQuery.ts
@@ -1,20 +1,20 @@
 import { DocumentData, onSnapshot, Query } from '@firebase/firestore';
 import { useEffect, useState } from 'react';
 
-export function useQuery<T>(query: Query<DocumentData>) {
-  const [data, setData] = useState<T>([] as unknown as T);
+export function useQuery<T>(query: Query<DocumentData>): T[] {
+  const [data, setData] = useState<T[]>([]);
 
   useEffect(() => {
     const unsubscribe = onSnapshot(query, (snapshot) => {
-      const docs: any[] = [];
+      const docs: T[] = [];
       snapshot.forEach((doc) => {
-        docs.push(doc.data());
+        docs.push(doc.data() as T);
       });
-      setData(docs as unknown as T);
+      setData(docs);
     });
 
     return unsubscribe;
   }, [query]);
 
-  return [data] as [T];
+  return data;
 }
