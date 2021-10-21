54339af4bfe1bfbf199d579ed02ca53f2a68cb92
joelblaser <joel.blaser.3@gmail.com>
2021-10-21 10:09:46 +0200

    feat(quiz): added title to sidebar

--- src/components/page-quiz/sidebar/Sidebar.tsx
+++ src/components/page-quiz/sidebar/Sidebar.tsx
@@ -1,3 +1,15 @@
-export function Sidebar() {
-  return <div></div>;
+import Typography from '@mui/material/Typography';
+
+interface Props {
+  title: string;
+}
+
+export function Sidebar({ title }: Props) {
+  return (
+    <div className="sidebar">
+      <Typography fontWeight="bold" variant="h6" component="p">
+        {title}
+      </Typography>
+    </div>
+  );
 }
